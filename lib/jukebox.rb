b783fc65666090ea33aa775cf63605b40048efef
Demarius Edwards <demarius.edwards@gmail.com>
2019-12-29 23:03:51 +0000

    Automatically backed up by Learn

--- lib/jukebox.rb
+++ lib/jukebox.rb
@@ -1,1 +1,63 @@
-# Add your code here
+# # Add your code here
+require 'pry'
+songs = [
+  "Phoenix - 1901",
+  "Tokyo Police Club - Wait Up",
+  "Sufjan Stevens - Too Much",
+  "The Naked and the Famous - Young Blood",
+  "(Far From) Home - Tiga",
+  "The Cults - Abducted",
+  "Phoenix - Consolation Prizes",
+  "Harry Chapin - Cats in the Cradle",
+  "Amos Lee - Keep It Loose, Keep It Tight"
+]
+
+# def say_hello(name)
+#   "Hi #{name}!"
+# end
+# puts "Enter your name:"
+# users_name = gets.strip
+# puts say_hello(users_name)
+
+def help
+  puts "I accept the following commands:/"
+  puts /- help : displays this help message/
+  puts /- list : displays a list of songs you can play/
+  puts /- play : lets you choose a song to play/
+  puts /- exit : exits this program/
+end
+
+def list(songs) # may have to use map method to change songs array
+   
+   songs.collect{|x| x}
+   binding.pry
+  # i = 0
+  # while i < songs.length do
+  #   puts "\/#{i+1}. #{songs[i]}"
+  #   i += 1
+  # end
+  # songs
+end
+
+def play(songs)
+  puts "Please enter a song name or number:"
+  # it's not grabbing list method but that 
+  #integer input should correspond to the index of the song
+  list(songs)
+  user_response = gets.strip.to_s
+  if songs.include?(user_response)
+    puts "Playing #{user_response}"
+  else
+    puts "Invalid input, please try again"
+  end
+end
+
+def exit_jukebox
+  puts "Goodbye"
+end
+
+def run
+  puts "Please enter a command:"
+  user_input = gets.strip
+end
+play(songs)
