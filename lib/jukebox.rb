d85703d587a83d511e31872da60516465205d3ba
Demarius Edwards <demarius.edwards@gmail.com>
2019-12-29 23:09:01 +0000

    Automatically backed up by Learn

--- lib/jukebox.rb
+++ lib/jukebox.rb
@@ -40,8 +40,9 @@ def play(songs)
   puts "Please enter a song name or number:"
   # it's not grabbing list method but that 
   #integer input should correspond to the index of the song
-  list(songs)
   binding.pry
+  list(songs)
+  
   user_response = gets.strip.to_s
   if songs.include?(user_response)
     puts "Playing #{user_response}"
